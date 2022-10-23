fa48240fc3b65c97c2ab79aaf119f67dcf9bf8eb
NessaAz <vanessa.azenwa@gmail.com>
2022-10-23 15:43:47 +0300

    create views function  for listings

--- listings/models.py
+++ listings/models.py
@@ -7,3 +7,6 @@ class Listing(models.Model):
     used = models.IntegerField()
     price = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
--- listings/views.py
+++ listings/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
 
+from .models import *
+
+def listings(request):
+
+    listings = Listing.objects.all()
+
+    context = {
+        'listings': listings,
+    }
+
+    return render(request, 'listings.html',context)
